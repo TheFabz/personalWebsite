f9025deed1d875e8d1a57c3eba6ba5183e0d7dc5
Fabio@Swogo <fabio@swogo.Home>
2021-06-07 15:00:06 +0100

    added static dir

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.use(express.static(__dirname+'/public'));
+
 // for use in cloud 
 app.listen(process.env.PORT, function () {
   console.log('listening.......................')
