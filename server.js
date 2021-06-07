b5f1b6d22a7ef29e8efc9372f40342bf610222bf
Fabio@Swogo <fabio@swogo.Home>
2021-06-07 14:51:41 +0100

    added files

--- server.js
+++ server.js
@@ -5,18 +5,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-/** for use in local machine 
+// for use in cloud 
 app.listen(process.env.PORT, function () {
-  console.log('listening on 3000')
+  console.log('listening.......................')
 })
-*/
 
+/*  
 //local use
 app.listen(3003, function () {
   console.log('listening on 3003')
 })
-
-
+*/
 
 //start page, loads log-in screen;
 app.get('/', (req, res) => {
