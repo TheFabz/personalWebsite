12c5bcb93adea2fb63f7fdc2256ee1ac2b53b2e3
Fabio Rodrigues <fabio.rodrigues@criticaltechworks.com>
2022-03-12 23:52:14 +0000

    refactored some code

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const emailSender = require('./email/emailSender');
+const emailBuilder = require('./email/emailBuilder');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(express.static(__dirname+'/'));
+
+app.post('/message', (req, res, next) => {
+  let reqData = req.body;
+  let message = emailBuilder.messageBuilder(reqData.userEmail,reqData.subject,reqData.message);
+  let confirmationMessage = emailBuilder.confirmationMessageBuilder(reqData.userEmail);
+
+  emailSender.sendMessage(message);
+  emailSender.sendMessage(confirmationMessage);
+  res.redirect('/')
+});
+
+//start page, loads log-in screen;
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+//local use
+// app.listen(3004, function () {
+//   console.log('listening on 3004')
+// })
+
+// for use in cloud 
+app.listen(process.env.PORT, function () {
+  console.log('listening.......................')
+})
+
